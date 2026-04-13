2444cc693baf04fe69a0d8991646f35d2638963c
Heshinth P <heshinth@gmail.com>
2026-04-13 21:42:06 +0530

    Rename HF_API_KEY to HF_TOKEN in settings for workinng

--- app/config.py
+++ app/config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
-    HF_API_KEY: SecretStr | None = None
-    """Hugging Face API key. Optional"""
+    HF_TOKEN: SecretStr | None = None
+    """Hugging Face API token. Optional"""
 
     DEFAULT_EMBEDDING_MODEL: str = "BAAI/bge-small-en-v1.5"
     """The default embedding model to use."""
